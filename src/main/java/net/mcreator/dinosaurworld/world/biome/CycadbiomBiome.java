e416307f27af396b825203d3bc448943a3233099
Jurko <Jurko@Trinity.advanced.sk>
2020-12-15 10:49:54 +0100

    edited cicad biom (cicad 'trees' wont spawn)

--- src/main/java/net/mcreator/dinosaurworld/world/biome/CycadbiomBiome.java
+++ src/main/java/net/mcreator/dinosaurworld/world/biome/CycadbiomBiome.java
@@ -11,35 +11,17 @@ import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
 import net.minecraft.world.gen.placement.Placement;
 import net.minecraft.world.gen.placement.FrequencyConfig;
-import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
 import net.minecraft.world.gen.feature.SphereReplaceConfig;
-import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.feature.GrassFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.AbstractTreeFeature;
-import net.minecraft.world.gen.IWorldGenerationReader;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.biome.DefaultBiomeFeatures;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.IWorldWriter;
-import net.minecraft.world.IWorld;
-import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.Direction;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.block.material.Material;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Block;
 
-import net.mcreator.dinosaurworld.block.CycadwoodBlock;
-import net.mcreator.dinosaurworld.block.CycadsaplingBlock;
-import net.mcreator.dinosaurworld.block.CyadleavesBlock;
 import net.mcreator.dinosaurworld.DinosaurworldElements;
-
-import java.util.Set;
-import java.util.Random;
 
 import com.google.common.collect.Lists;
 
@@ -76,8 +58,6 @@ public class CycadbiomBiome extends DinosaurworldElements.ModElement {
 					new GrassFeatureConfig(Blocks.GRASS.getDefaultState()), Placement.COUNT_HEIGHTMAP_DOUBLE, new FrequencyConfig(3)));
 			addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(Feature.DEAD_BUSH,
 					IFeatureConfig.NO_FEATURE_CONFIG, Placement.COUNT_HEIGHTMAP_DOUBLE, new FrequencyConfig(3)));
-			addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(new CustomTreeFeature(),
-					IFeatureConfig.NO_FEATURE_CONFIG, Placement.COUNT_EXTRA_HEIGHTMAP, new AtSurfaceWithExtraConfig(3, 0.1F, 1)));
 			addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(Feature.CACTUS, IFeatureConfig.NO_FEATURE_CONFIG,
 					Placement.COUNT_HEIGHTMAP_DOUBLE, new FrequencyConfig(2)));
 			addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
@@ -108,122 +88,6 @@ public class CycadbiomBiome extends DinosaurworldElements.ModElement {
 		@Override
 		public int getSkyColorByTemp(float currentTemperature) {
 			return -1;
-		}
-	}
-
-	static class CustomTreeFeature extends AbstractTreeFeature<NoFeatureConfig> {
-		CustomTreeFeature() {
-			super(NoFeatureConfig::deserialize, false);
-		}
-
-		@Override
-		public boolean place(Set<BlockPos> changedBlocks, IWorldGenerationReader worldgen, Random rand, BlockPos position, MutableBoundingBox bbox) {
-			if (!(worldgen instanceof IWorld))
-				return false;
-			IWorld world = (IWorld) worldgen;
-			int height = rand.nextInt(5) + 1;
-			boolean spawnTree = true;
-			if (position.getY() >= 1 && position.getY() + height + 1 <= world.getHeight()) {
-				for (int j = position.getY(); j <= position.getY() + 1 + height; j++) {
-					int k = 1;
-					if (j == position.getY())
-						k = 0;
-					if (j >= position.getY() + height - 1)
-						k = 2;
-					for (int px = position.getX() - k; px <= position.getX() + k && spawnTree; px++) {
-						for (int pz = position.getZ() - k; pz <= position.getZ() + k && spawnTree; pz++) {
-							if (j >= 0 && j < world.getHeight()) {
-								if (!this.isReplaceable(world, new BlockPos(px, j, pz))) {
-									spawnTree = false;
-								}
-							} else {
-								spawnTree = false;
-							}
-						}
-					}
-				}
-				if (!spawnTree) {
-					return false;
-				} else {
-					Block ground = world.getBlockState(position.add(0, -1, 0)).getBlock();
-					Block ground2 = world.getBlockState(position.add(0, -2, 0)).getBlock();
-					if (!((ground == Blocks.GRASS_BLOCK.getDefaultState().getBlock() || ground == Blocks.SAND.getDefaultState().getBlock())
-							&& (ground2 == Blocks.GRASS_BLOCK.getDefaultState().getBlock() || ground2 == Blocks.SAND.getDefaultState().getBlock())))
-						return false;
-					BlockState state = world.getBlockState(position.down());
-					if (position.getY() < world.getHeight() - height - 1) {
-						setTreeBlockState(changedBlocks, world, position.down(), Blocks.SAND.getDefaultState(), bbox);
-						for (int genh = position.getY() - 3 + height; genh <= position.getY() + height; genh++) {
-							int i4 = genh - (position.getY() + height);
-							int j1 = (int) (1 - i4 * 0.5);
-							for (int k1 = position.getX() - j1; k1 <= position.getX() + j1; ++k1) {
-								for (int i2 = position.getZ() - j1; i2 <= position.getZ() + j1; ++i2) {
-									int j2 = i2 - position.getZ();
-									if (Math.abs(position.getX()) != j1 || Math.abs(j2) != j1 || rand.nextInt(2) != 0 && i4 != 0) {
-										BlockPos blockpos = new BlockPos(k1, genh, i2);
-										state = world.getBlockState(blockpos);
-										if (state.getBlock().isAir(state, world, blockpos) || state.getMaterial().blocksMovement()
-												|| state.isIn(BlockTags.LEAVES) || state.getBlock() == Blocks.AIR.getDefaultState().getBlock()
-												|| state.getBlock() == CyadleavesBlock.block.getDefaultState().getBlock()) {
-											setTreeBlockState(changedBlocks, world, blockpos, CyadleavesBlock.block.getDefaultState(), bbox);
-										}
-									}
-								}
-							}
-						}
-						for (int genh = 0; genh < height; genh++) {
-							BlockPos genhPos = position.up(genh);
-							state = world.getBlockState(genhPos);
-							setTreeBlockState(changedBlocks, world, genhPos, CycadwoodBlock.block.getDefaultState(), bbox);
-							if (state.getBlock().isAir(state, world, genhPos) || state.getMaterial().blocksMovement() || state.isIn(BlockTags.LEAVES)
-									|| state.getBlock() == Blocks.AIR.getDefaultState().getBlock()
-									|| state.getBlock() == CyadleavesBlock.block.getDefaultState().getBlock()) {
-							}
-						}
-						if (rand.nextInt(4) == 0 && height > 5) {
-							for (int hlevel = 0; hlevel < 2; hlevel++) {
-								for (Direction Direction : Direction.Plane.HORIZONTAL) {
-									if (rand.nextInt(4 - hlevel) == 0) {
-										Direction dir = Direction.getOpposite();
-										setTreeBlockState(changedBlocks, world, position.add(dir.getXOffset(), height - 5 + hlevel, dir.getZOffset()),
-												CycadsaplingBlock.block.getDefaultState(), bbox);
-									}
-								}
-							}
-						}
-						return true;
-					} else {
-						return false;
-					}
-				}
-			} else {
-				return false;
-			}
-		}
-
-		private void addVines(IWorld world, BlockPos pos, Set<BlockPos> changedBlocks, MutableBoundingBox bbox) {
-			setTreeBlockState(changedBlocks, world, pos, Blocks.AIR.getDefaultState(), bbox);
-			int i = 5;
-			for (BlockPos blockpos = pos.down(); world.isAirBlock(blockpos) && i > 0; --i) {
-				setTreeBlockState(changedBlocks, world, blockpos, Blocks.AIR.getDefaultState(), bbox);
-				blockpos = blockpos.down();
-			}
-		}
-
-		private boolean canGrowInto(Block blockType) {
-			return blockType.getDefaultState().getMaterial() == Material.AIR || blockType == CycadwoodBlock.block.getDefaultState().getBlock()
-					|| blockType == CyadleavesBlock.block.getDefaultState().getBlock() || blockType == Blocks.GRASS_BLOCK.getDefaultState().getBlock()
-					|| blockType == Blocks.SAND.getDefaultState().getBlock();
-		}
-
-		private boolean isReplaceable(IWorld world, BlockPos pos) {
-			BlockState state = world.getBlockState(pos);
-			return state.getBlock().isAir(state, world, pos) || canGrowInto(state.getBlock()) || !state.getMaterial().blocksMovement();
-		}
-
-		private void setTreeBlockState(Set<BlockPos> changedBlocks, IWorldWriter world, BlockPos pos, BlockState state, MutableBoundingBox mbb) {
-			super.setLogState(changedBlocks, world, pos, state, mbb);
-			changedBlocks.add(pos.toImmutable());
 		}
 	}
 }
