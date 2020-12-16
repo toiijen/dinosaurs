889be571ebd3d3ba0fed0117ac6f8f3241511780
Jurko <Jurko@Trinity.advanced.sk>
2020-12-16 14:08:15 +0100

    fixed electric fence corner

--- src/main/java/net/mcreator/dinosaurworld/block/PlortrohBlock.java
+++ src/main/java/net/mcreator/dinosaurworld/block/PlortrohBlock.java
@@ -8,22 +8,18 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraft.world.storage.loot.LootContext;
 import net.minecraft.world.World;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.Rotation;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Direction;
 import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.state.StateContainer;
-import net.minecraft.state.DirectionProperty;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
-import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.BlockItem;
 import net.minecraft.entity.Entity;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.DirectionalBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
@@ -34,10 +30,10 @@ import java.util.List;
 import java.util.Collections;
 
 @DinosaurworldElements.ModElement.Tag
-public class PlotvrhrohBlock extends DinosaurworldElements.ModElement {
-	@ObjectHolder("dinosaurworld:plotvrhroh")
+public class PlortrohBlock extends DinosaurworldElements.ModElement {
+	@ObjectHolder("dinosaurworld:plortroh")
 	public static final Block block = null;
-	public PlotvrhrohBlock(DinosaurworldElements instance) {
+	public PlortrohBlock(DinosaurworldElements instance) {
 		super(instance, 23);
 	}
 
@@ -48,11 +44,9 @@ public class PlotvrhrohBlock extends DinosaurworldElements.ModElement {
 				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
 	}
 	public static class CustomBlock extends Block {
-		public static final DirectionProperty FACING = DirectionalBlock.FACING;
 		public CustomBlock() {
 			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(1f, 20f).lightValue(0));
-			this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
-			setRegistryName("plotvrhroh");
+			setRegistryName("plortroh");
 		}
 
 		@OnlyIn(Dist.CLIENT)
@@ -62,26 +56,18 @@ public class PlotvrhrohBlock extends DinosaurworldElements.ModElement {
 		}
 
 		@Override
+		public boolean isNormalCube(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return false;
+		}
+
+		@Override
 		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
 			return true;
 		}
 
 		@Override
-		protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-			builder.add(FACING);
-		}
-
-		public BlockState rotate(BlockState state, Rotation rot) {
-			return state.with(FACING, rot.rotate(state.get(FACING)));
-		}
-
-		public BlockState mirror(BlockState state, Mirror mirrorIn) {
-			return state.rotate(mirrorIn.toRotation(state.get(FACING)));
-		}
-
-		@Override
-		public BlockState getStateForPlacement(BlockItemUseContext context) {
-			return this.getDefaultState().with(FACING, context.getNearestLookingDirection().getOpposite());
+		public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
+			return VoxelShapes.create(0D, 0D, 0D, 0.625D, 1D, 0.625D);
 		}
 
 		@Override
