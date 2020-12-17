2d8821db489ffe78018d5c091e8a6f823fceafce
Jurko <Jurko@Trinity.advanced.sk>
2020-12-17 09:06:34 +0100

    added ankylosaurus animation

--- models/Modelankylosaurus.java
+++ models/Modelankylosaurus.java
@@ -1,64 +1,74 @@
 
-public static class Modelankylosaurus extends EntityModel {
-	private final RendererModel tail1;
-	private final RendererModel tail2;
-	private final RendererModel tail3;
-	private final RendererModel body;
-	private final RendererModel rightlegs;
-	private final RendererModel leftlegs;
-	private final RendererModel head;
+public static class Modelankylosaurus extends ModelBase {
+	private final ModelRenderer leftbackleg;
+	private final ModelRenderer leftfrontleg;
+	private final ModelRenderer rightbackleg;
+	private final ModelRenderer rightfrontleg;
+	private final ModelRenderer tail;
+	private final ModelRenderer tail2;
+	private final ModelRenderer tail3;
+	private final ModelRenderer body;
+	private final ModelRenderer head;
 
 	public Modelankylosaurus() {
 		textureWidth = 128;
 		textureHeight = 128;
 
-		tail1 = new RendererModel(this);
-		tail1.setRotationPoint(0.0F, 15.75F, 29.0F);
-		tail1.cubeList.add(new ModelBox(tail1, 0, 36, -1.5F, -1.75F, 0.0F, 3, 3, 6, 0.0F, false));
+		leftbackleg = new ModelRenderer(this);
+		leftbackleg.setRotationPoint(9.5F, 18.0F, 12.5F);
+		leftbackleg.cubeList.add(new ModelBox(leftbackleg, 0, 51, -1.5F, 0.0F, -1.5F, 3, 6, 3, 0.0F, false));
 
-		tail2 = new RendererModel(this);
-		tail2.setRotationPoint(0.0F, -0.25F, 6.0F);
-		tail1.addChild(tail2);
-		tail2.cubeList.add(new ModelBox(tail2, 12, 22, -1.5F, -1.5F, 0.0F, 3, 3, 6, 0.0F, false));
+		leftfrontleg = new ModelRenderer(this);
+		leftfrontleg.setRotationPoint(9.5F, 18.0F, -13.5F);
+		leftfrontleg.cubeList.add(new ModelBox(leftfrontleg, 59, 66, -1.5F, 0.0F, -1.5F, 3, 6, 3, 0.0F, false));
 
-		tail3 = new RendererModel(this);
-		tail3.setRotationPoint(0.0F, 0.0F, 6.0F);
+		rightbackleg = new ModelRenderer(this);
+		rightbackleg.setRotationPoint(-9.5F, 18.0F, 12.5F);
+		rightbackleg.cubeList.add(new ModelBox(rightbackleg, 13, 48, -1.5F, 0.0F, -1.5F, 3, 6, 3, 0.0F, false));
+
+		rightfrontleg = new ModelRenderer(this);
+		rightfrontleg.setRotationPoint(-9.5F, 18.0F, -13.5F);
+		rightfrontleg.cubeList.add(new ModelBox(rightfrontleg, 0, 66, -1.5F, 0.0F, -1.5F, 3, 6, 3, 0.0F, false));
+
+		tail = new ModelRenderer(this);
+		tail.setRotationPoint(0.0F, 15.75F, 15.0F);
+		tail.cubeList.add(new ModelBox(tail, 0, 36, -1.5F, -1.75F, 0.0F, 3, 3, 6, 0.0F, false));
+
+		tail2 = new ModelRenderer(this);
+		tail2.setRotationPoint(0.0F, 0.0F, 6.0F);
+		tail.addChild(tail2);
+		tail2.cubeList.add(new ModelBox(tail2, 12, 22, -1.5F, -1.75F, 0.0F, 3, 3, 6, 0.0F, false));
+
+		tail3 = new ModelRenderer(this);
+		tail3.setRotationPoint(0.5F, -0.75F, 6.0F);
 		tail2.addChild(tail3);
-		tail3.cubeList.add(new ModelBox(tail3, 0, 19, -1.5F, -1.5F, 0.0F, 3, 3, 6, 0.0F, false));
-		tail3.cubeList.add(new ModelBox(tail3, 0, 0, -4.0F, -2.0F, 6.0F, 8, 4, 5, 0.0F, false));
+		tail3.cubeList.add(new ModelBox(tail3, 0, 0, -4.5F, -1.5F, 6.0F, 8, 4, 5, 0.0F, false));
+		tail3.cubeList.add(new ModelBox(tail3, 0, 19, -2.0F, -1.0F, 0.0F, 3, 3, 6, 0.0F, false));
 
-		body = new RendererModel(this);
-		body.setRotationPoint(0.0F, 10.0F, 2.0F);
-		body.cubeList.add(new ModelBox(body, 0, 0, -13.0F, 3.0F, -4.0F, 26, 5, 31, 0.0F, false));
-		body.cubeList.add(new ModelBox(body, 0, 36, -11.0F, 0.0F, -2.0F, 22, 3, 27, 0.0F, false));
-		body.cubeList.add(new ModelBox(body, 0, 66, -9.0F, -1.0F, 0.0F, 18, 1, 23, 0.0F, false));
+		body = new ModelRenderer(this);
+		body.setRotationPoint(0.0F, 12.1667F, -0.5F);
+		body.cubeList.add(new ModelBox(body, 0, 0, -13.0F, 0.8333F, -15.5F, 26, 5, 31, 0.0F, false));
+		body.cubeList.add(new ModelBox(body, 0, 36, -11.0F, -2.1667F, -13.5F, 22, 3, 27, 0.0F, false));
+		body.cubeList.add(new ModelBox(body, 0, 66, -9.0F, -3.1667F, -11.5F, 18, 1, 23, 0.0F, false));
 
-		rightlegs = new RendererModel(this);
-		rightlegs.setRotationPoint(-9.0F, 24.0F, 25.0F);
-		rightlegs.cubeList.add(new ModelBox(rightlegs, 0, 66, -2.0F, -6.0F, -26.0F, 3, 6, 3, 0.0F, false));
-		rightlegs.cubeList.add(new ModelBox(rightlegs, 13, 48, -2.0F, -6.0F, 0.0F, 3, 6, 3, 0.0F, false));
-
-		leftlegs = new RendererModel(this);
-		leftlegs.setRotationPoint(9.0F, 24.0F, 25.0F);
-		leftlegs.cubeList.add(new ModelBox(leftlegs, 59, 66, -1.0F, -6.0F, -26.0F, 3, 6, 3, 0.0F, false));
-		leftlegs.cubeList.add(new ModelBox(leftlegs, 0, 51, -1.0F, -6.0F, 0.0F, 3, 6, 3, 0.0F, false));
-
-		head = new RendererModel(this);
-		head.setRotationPoint(-0.5F, 15.5F, -2.15F);
-		head.cubeList.add(new ModelBox(head, 0, 45, -2.5F, -1.5F, -2.85F, 5, 3, 3, 0.0F, false));
-		head.cubeList.add(new ModelBox(head, 0, 9, -3.0F, -2.0F, -8.85F, 6, 4, 6, 0.0F, false));
+		head = new ModelRenderer(this);
+		head.setRotationPoint(-0.5F, 15.5F, -16.0F);
+		head.cubeList.add(new ModelBox(head, 0, 45, -2.5F, -1.5F, -3.0F, 5, 3, 3, 0.0F, false));
+		head.cubeList.add(new ModelBox(head, 0, 9, -3.0F, -2.0F, -9.0F, 6, 4, 6, 0.0F, false));
 	}
 
 	@Override
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-		tail1.render(f5);
+		leftbackleg.render(f5);
+		leftfrontleg.render(f5);
+		rightbackleg.render(f5);
+		rightfrontleg.render(f5);
+		tail.render(f5);
 		body.render(f5);
-		rightlegs.render(f5);
-		leftlegs.render(f5);
 		head.render(f5);
 	}
 
-	public void setRotationAngle(RendererModel modelRenderer, float x, float y, float z) {
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
 		modelRenderer.rotateAngleX = x;
 		modelRenderer.rotateAngleY = y;
 		modelRenderer.rotateAngleZ = z;
@@ -66,5 +76,14 @@ public static class Modelankylosaurus extends EntityModel {
 
 	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity e) {
 		super.setRotationAngles(f, f1, f2, f3, f4, f5, e);
+		this.head.rotateAngleY = f3 / (180F / (float) Math.PI);
+		this.head.rotateAngleX = f4 / (180F / (float) Math.PI);
+		this.leftfrontleg.rotateAngleX = MathHelper.cos(f * 1.0F) * -1.0F * f1;
+		this.rightbackleg.rotateAngleX = MathHelper.cos(f * 1.0F) * 1.0F * f1;
+		this.tail2.rotateAngleY = MathHelper.cos(f * 0.6662F) * f1;
+		this.tail3.rotateAngleY = MathHelper.cos(f * 0.6662F) * f1;
+		this.tail.rotateAngleY = MathHelper.cos(f * 0.6662F) * f1;
+		this.leftbackleg.rotateAngleX = MathHelper.cos(f * 1.0F) * -1.0F * f1;
+		this.rightfrontleg.rotateAngleX = MathHelper.cos(f * 1.0F) * 1.0F * f1;
 	}
 }
