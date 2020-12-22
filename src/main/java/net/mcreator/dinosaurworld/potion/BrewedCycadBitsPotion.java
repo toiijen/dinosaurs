ae7756ed0ce78a46173d10c4d456f66094af13f4
Stanke <Stanke@Darwin>
2020-12-22 16:34:27 +0100

    added brewed cycad bits. adds agility and health with initial nausea

---
+++ src/main/java/net/mcreator/dinosaurworld/potion/BrewedCycadBitsPotion.java
@@ -0,0 +1,111 @@
+
+package net.mcreator.dinosaurworld.potion;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.EffectType;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effect;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
+import net.minecraft.entity.LivingEntity;
+
+import net.mcreator.dinosaurworld.procedures.BrewedCycadBitsPotionStartedappliedProcedure;
+import net.mcreator.dinosaurworld.DinosaurworldElements;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@DinosaurworldElements.ModElement.Tag
+public class BrewedCycadBitsPotion extends DinosaurworldElements.ModElement {
+	@ObjectHolder("dinosaurworld:brewedcycadbits")
+	public static final Effect potion = null;
+	@ObjectHolder("dinosaurworld:brewedcycadbits")
+	public static final Potion potionType = null;
+	public BrewedCycadBitsPotion(DinosaurworldElements instance) {
+		super(instance, 51);
+		FMLJavaModLoadingContext.get().getModEventBus().register(this);
+	}
+
+	@SubscribeEvent
+	public void registerEffect(RegistryEvent.Register<Effect> event) {
+		event.getRegistry().register(new EffectCustom());
+	}
+
+	@SubscribeEvent
+	public void registerPotion(RegistryEvent.Register<Potion> event) {
+		event.getRegistry().register(new PotionCustom());
+	}
+	public static class PotionCustom extends Potion {
+		public PotionCustom() {
+			super(new EffectInstance(potion, 3600));
+			setRegistryName("brewedcycadbits");
+		}
+	}
+
+	public static class EffectCustom extends Effect {
+		private final ResourceLocation potionIcon;
+		public EffectCustom() {
+			super(EffectType.BENEFICIAL, -6684775);
+			setRegistryName("brewedcycadbits");
+			potionIcon = new ResourceLocation("dinosaurworld:textures/brewed_cycad_stuff.png");
+		}
+
+		@Override
+		public String getName() {
+			return "effect.brewedcycadbits";
+		}
+
+		@Override
+		public boolean isBeneficial() {
+			return true;
+		}
+
+		@Override
+		public boolean isInstant() {
+			return false;
+		}
+
+		@Override
+		public List<ItemStack> getCurativeItems() {
+			List<ItemStack> ret = new ArrayList<>();
+			ret.add(new ItemStack(Items.MILK_BUCKET, (int) (1)));
+			return ret;
+		}
+
+		@Override
+		public boolean shouldRenderInvText(EffectInstance effect) {
+			return true;
+		}
+
+		@Override
+		public boolean shouldRenderHUD(EffectInstance effect) {
+			return true;
+		}
+
+		@Override
+		public void applyAttributesModifiersToEntity(LivingEntity entity, AbstractAttributeMap attributeMapIn, int amplifier) {
+			World world = entity.world;
+			int x = (int) entity.posX;
+			int y = (int) entity.posY;
+			int z = (int) entity.posZ;
+			{
+				java.util.HashMap<String, Object> $_dependencies = new java.util.HashMap<>();
+				$_dependencies.put("entity", entity);
+				BrewedCycadBitsPotionStartedappliedProcedure.executeProcedure($_dependencies);
+			}
+		}
+
+		@Override
+		public boolean isReady(int duration, int amplifier) {
+			return true;
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/dinosaurworld/procedures/BrewedCycadBitsPotionStartedappliedProcedure.java
@@ -0,0 +1,31 @@
+package net.mcreator.dinosaurworld.procedures;
+
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.dinosaurworld.DinosaurworldElements;
+
+@DinosaurworldElements.ModElement.Tag
+public class BrewedCycadBitsPotionStartedappliedProcedure extends DinosaurworldElements.ModElement {
+	public BrewedCycadBitsPotionStartedappliedProcedure(DinosaurworldElements instance) {
+		super(instance, 51);
+	}
+
+	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			System.err.println("Failed to load dependency entity for procedure BrewedCycadBitsPotionStartedapplied!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.NAUSEA, (int) 30, (int) 15));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SPEED, (int) 6200, (int) 5));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.JUMP_BOOST, (int) 6200, (int) 3));
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).setHealth((float) (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHealth() : -1) + 2));
+	}
+}
